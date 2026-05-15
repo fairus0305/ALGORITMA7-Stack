a651783d26f5d176d4fcc128d5d30c17a6ddaf50
fairus0305 <ruszezzy@gmail.com>
2026-05-15 15:09:59 +0700

    case 4 untuk krluar dari program

--- stack.cpp
+++ stack.cpp
@@ -102,6 +102,17 @@ int main()
             case 3:
                 stack.peek();
                 break;
+
+            case 4:
+                cout << "Exiting program. " << endl;
+                break;
+            default:
+                cout << "invalid choice. try again." << endl;
+                break;
             }
+
+            cout << endl;
     }
+
+    return 0;
 }
