3817296a546093c0c371d3efde492fa147e10b1a
fairus0305 <ruszezzy@gmail.com>
2026-05-15 14:38:08 +0700

    Menambahkan atribut data dan pointer next

--- stack.cpp
+++ stack.cpp
@@ -4,5 +4,13 @@ using namespace std;
 //Node class representing a singgke node in the linked list
 class Node
 {
-    
-}
+    public :
+        int data;
+        Node *next;
+
+        Node()
+        {
+            next = NULL;
+        }
+};
+
